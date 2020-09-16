0441b88bad9e3c3ec3dc6ee2e3663cbfdd03ed46
HeeyeonJeong <HeeyeonJeong>
2020-09-16 22:57:14 +0900

    저장된 latitude, longitude 날씨 정보 추가

--- weather.js
+++ weather.js
@@ -1,4 +1,22 @@
+const weather = document.querySelector(".js-weather");
+const API_key = "2bbe30ef9fb2f7435327c085632b1349";
 const COORDS = "coords";
+
+function getWeather(lat, lon) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_key}&units=metric`
+  )
+    .then(function (reponse) {
+      return reponse.json();
+    })
+    .then(function (json) {
+      console.log(json);
+      const temperature = json.main.temp;
+      const place = json.name;
+      const type = json.weather[0].main;
+      weather.innerHTML = `${temperature}°C / ${type} / ${place}`;
+    });
+}
 
 function saveCoords(coordsObj) {
   localStorage.setItem(COORDS, JSON.stringify(coordsObj));
@@ -12,6 +30,7 @@ function handleGeoSucces(position) {
     longitude,
   };
   saveCoords(coordsObj);
+  getWeather(latitude, longitude);
 }
 
 function handleGeoError(position) {
